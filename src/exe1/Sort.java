6f5d45eb093be8e9c2d0e1b01f4a2d48e5d799cc
alvaroDantas <alvarodantas1997@hotmail.com>
2019-09-16 19:35:27 -0300

    arquivo final

---
+++ src/exe1/Sort.java
@@ -0,0 +1,12 @@
+package exe1;
+
+/**
+ *
+ * @author DLT1CA
+ */
+public abstract class Sort {
+
+    abstract Funcionario[] sortAsc(Funcionario[] funcionarios);
+
+    abstract Funcionario[] sortDesc(Funcionario[] funcionarios);
+}
